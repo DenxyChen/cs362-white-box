541fc4d63a9cec6d742c3da00a9f9e10a454a637
DenxyChen <denxychen@gmail.com>
2022-07-10 11:48:01 -0700

    init tests

--- tests.py
+++ tests.py
@@ -3,5 +3,29 @@
 # Date: 7/8/2022
 
 from contrived_func import contrived_func
+import unittest
 
+class ContrivedFuncCoverageTests(unittest.TestCase):
+    def test_c1(self):
+        val = 125
+        self.assertTrue(contrived_func(val))
 
+    def test_c2(self):
+        val = 6
+        self.assertFalse(contrived_func(val))
+
+    def test_c3(self):
+        val = 5
+        self.assertTrue(contrived_func(val))
+
+    def test_c4(self):
+        val = 59
+        self.assertFalse(contrived_func(val))
+
+    def test_c5(self):
+        val = 99
+        self.assertFalse(contrived_func(val))
+
+    def test_c6(self):
+        val = 250
+        self.assertTrue(contrived_func(val))
